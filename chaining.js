7e37f4b9b37a0dc84061139a2acdacc8fdc60e53
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-02-22 10:17:16 -0600

    Files added. Just started.

---
+++ chaining.js
@@ -0,0 +1,8 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+var revOrder = integers.reverse(); 
+
+// Remove any integers greater than 19.
+// Multiply each remaining number by 1.5 and then substract 1.
+// Then output (either in the DOM or the console) the sum of all the resulting numbers.
